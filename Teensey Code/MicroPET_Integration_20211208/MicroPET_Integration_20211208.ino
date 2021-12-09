e1ff78950c502fff9d71b6d6b1d3286770e035de
xxxxxxxxxinliu <xin@slowimmediate.com>
2021-12-08 20:45:50 -0500

    integration

--- Teensey Code/MicroPET_Integration_20211208/MicroPET_Integration_20211208.ino
+++ Teensey Code/MicroPET_Integration_20211208/MicroPET_Integration_20211208.ino
@@ -22,8 +22,11 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 #include <Adafruit_AS726x.h>
+#include <Adafruit_MCP23X17.h>
 
 //Teensey 4.1 - SCL 19 - yellow /SDA 18 - blue 
+
+//sensor
 
 
 #define BME_SCK 13
@@ -49,7 +52,115 @@ File myFile;
 const int chipSelect = BUILTIN_SDCARD;
 const int ledPin = 5;
 
+//IO ports
 
+#define MCP_ADDR0 0x20  // IC5 on base board
+#define MCP_ADDR1 0x21  // IC4 on base board
+#define MCP_ADDR2 0x22  // IC1 on sensor board
+
+#define LED 13
+
+// Double check these pin assignments
+#define GPA0 0
+#define GPA1 1
+#define GPA2 2
+#define GPA3 3
+#define GPA4 4
+#define GPA5 5
+#define GPA6 6
+#define GPA7 7
+
+#define GPB0 8
+#define GPB1 9
+#define GPB2 10
+#define GPB3 11
+#define GPB4 12
+#define GPB5 13
+#define GPB6 14
+#define GPB7 15
+
+#define experimentOne 1
+#define experimentTwo 2
+
+#define e_buffer 0
+#define enzyme 1
+#define chamberA 2
+#define media 3
+#define chamberB 4
+#define waste 5
+#define preservativeOne 6  
+#define preservativeTwo 7
+#define preservativeThree 8
+#define preservativeFour 9
+#define preservativeFive 10
+#define preservativeSix 11
+
+
+Adafruit_MCP23X17 mcp0;
+Adafruit_MCP23X17 mcp1;
+Adafruit_MCP23X17 mcp2;
+
+typedef enum {
+  // MCP0 - IC5 on base board
+  BASEBD_J4_PIN2 = 0,
+  BASEBD_J4_PIN4,
+  BASEBD_J3_PIN2,
+  BASEBD_J3_PIN4,
+  BASEBD_J2_PIN2,
+  BASEBD_J2_PIN4,
+  BASEBD_J1_PIN2,
+  BASEBD_J1_PIN4,
+  BASEBD_J5_PIN2,
+  BASEBD_J5_PIN4,
+  BASEBD_J6_PIN2,
+  BASEBD_J6_PIN4,
+  BASEBD_J7_PIN2,
+  BASEBD_J7_PIN4,
+  BASEBD_J8_PIN2,
+  BASEBD_J8_PIN4,
+
+  // MCP1 - IC4 on base board
+  BASEBD_J12_PIN2,
+  BASEBD_J12_PIN4,
+  BASEBD_J11_PIN2,
+  BASEBD_J11_PIN4,
+  BASEBD_J10_PIN2,
+  BASEBD_J10_PIN4,
+  BASEBD_J9_PIN2,
+  BASEBD_J9_PIN4,
+  
+  //MCP2 - IC1 on sensor board
+  SENSEBD_J2_PIN1,
+  SENSEBD_J2_PIN2,
+  SENSEBD_J2_PIN3,
+  SENSEBD_J2_PIN4,
+  SENSEBD_J3_PIN1,
+  SENSEBD_J3_PIN2
+} CONN_PIN_T; // Connector and Pin type
+
+#define MCP1_PIN_OFFSET 16
+#define MCP2_PIN_OFFSET 16
+
+/****************************************/
+
+boolean switch_conn_pin(CONN_PIN_T conn_pin, byte level)
+{
+  if((level != HIGH) && (level != LOW)) {
+    return false;
+  }
+  
+  if(conn_pin <= BASEBD_J8_PIN4) {
+    mcp0.digitalWrite(conn_pin, level);
+  } else if(conn_pin <= BASEBD_J9_PIN4) {
+    mcp1.digitalWrite((conn_pin - MCP1_PIN_OFFSET), level);
+  } else if(conn_pin <= SENSEBD_J3_PIN2) {
+    mcp2.digitalWrite((conn_pin - MCP2_PIN_OFFSET), level);
+  } else {
+    return false;
+  }
+
+  return true;
+}
 
 
 void setup() {
@@ -62,6 +173,65 @@ void setup() {
     while(!Serial);    // time to get serial running
     Serial.println(F("Begin Serial"));
 
+    pinMode(LED, OUTPUT);
+// uncomment appropriate mcp.begin
+  if (!mcp0.begin_I2C(MCP_ADDR0)) {
+    Serial.println("Error initializing chip 0.");
+    while (1);
+  }
+
+  if (!mcp1.begin_I2C(MCP_ADDR1)) {
+    Serial.println("Error initializing chip 1.");
+    while (1);
+  }
+
+  // IC5 on base board
+  mcp0.pinMode(GPA0, OUTPUT);
+  mcp0.pinMode(GPA1, OUTPUT);
+  mcp0.pinMode(GPA2, OUTPUT);
+  mcp0.pinMode(GPA3, OUTPUT);
+  mcp0.pinMode(GPA4, OUTPUT);
+  mcp0.pinMode(GPA5, OUTPUT);
+  mcp0.pinMode(GPA6, OUTPUT);
+  mcp0.pinMode(GPA7, OUTPUT);
+
+  mcp0.pinMode(GPB0, OUTPUT);
+  mcp0.pinMode(GPB1, OUTPUT);
+  mcp0.pinMode(GPB2, OUTPUT);
+  mcp0.pinMode(GPB3, OUTPUT);
+  mcp0.pinMode(GPB4, OUTPUT);
+  mcp0.pinMode(GPB5, OUTPUT);
+  mcp0.pinMode(GPB6, OUTPUT);
+  mcp0.pinMode(GPB7, OUTPUT);
+
+  // IC4 on base board
+  mcp1.pinMode(GPA0, OUTPUT);
+  mcp1.pinMode(GPA1, OUTPUT);
+  mcp1.pinMode(GPA2, OUTPUT);
+  mcp1.pinMode(GPA3, OUTPUT);
+  mcp1.pinMode(GPA4, OUTPUT);
+  mcp1.pinMode(GPA5, OUTPUT);
+  mcp1.pinMode(GPA6, OUTPUT);
+  mcp1.pinMode(GPA7, OUTPUT);
+
+/*****************************************************
+ * Note - comment out below if only using base board
+ 
+   if (!mcp2.begin_I2C(MCP_ADDR2)) {
+    Serial.println("Error initializing chip 2.");
+    while (1);
+  }
+
+  // IC1 on sensor board
+  mcp2.pinMode(GPB0, OUTPUT);
+  mcp2.pinMode(GPB1, OUTPUT);
+  mcp2.pinMode(GPB2, OUTPUT);
+  mcp2.pinMode(GPB3, OUTPUT);
+  mcp2.pinMode(GPB4, OUTPUT);
+  mcp2.pinMode(GPB5, OUTPUT);
+
+  Serial.println("Looping...");
+*/
     unsigned bme_status;
     unsigned ams_status;
     // default settings
@@ -108,9 +278,102 @@ void loop() {
 }
 
 
-void day_1(){
-  Serial.println("Experimental_date");    
+void day_1(){ 
+/*####Day1 (Dec 9)
+#Revival Chamber to Chamber B - ?ml (done manually)
+#Start chamber A
+#open waste valve
+#enzyme to chamber A - 0.06 ml
+#buffer to chamber A - 2.94 ml (flexible)
+*/
+
+Serial.println("start moving liquid");
+switchCollection(experimentOne, waste);
+moveLiquid (experimentOne, enzyme, chamberA, 75);
+moveLiquid (experimentOne, e_buffer, chamberA, 2925);
+Serial.println("finished moving liquid");
 }
+
+void day_2(){
+  }
+
+void day_3(){
+  }
+
+void day_4(){
+  }
+
+void day_5(){
+  }
+
+
+void moveLiquid(int experiment, int origin, int target, float liquid_volume) 
+{
+     if (experiment == 1 && origin == 0)
+     {
+      switch_conn_pin(BASEBD_J10_PIN2, LOW); //Experiment 1 valveA
+      Serial.println("move from e_buffer");
+      }
+
+     if (experiment == 1 && origin == 1)
+     {
+      switch_conn_pin(BASEBD_J10_PIN2, HIGH); //Experiment 1 valveA
+      Serial.println("move from enzyme");
+      }
+
+     if (experiment == 1 && target == 2)
+     {
+      int move_pulse = liquid_volume / 25; //1ml = 1000uL and the pump moves 25uL at a time
+
+      for (int i = 0; i <= move_pulse; i++){
+      switch_conn_pin(BASEBD_J10_PIN4, HIGH); //Experiment 1 pumpA
+      digitalWrite(LED, HIGH);
+      delay(200);
+      switch_conn_pin(BASEBD_J10_PIN4, LOW); //Experiment 1 pumpA
+      digitalWrite(LED, LOW);
+      delay(200);
+       Serial.println( i * 25);
+      }
+      switch_conn_pin(BASEBD_J10_PIN2, LOW); //Reset xperiment 1 valveA so enzyme is blocked
+     }
+
+     if (experiment == 1 && origin == 3) // media going out
+     {
+      switch_conn_pin(BASEBD_J9_PIN2, LOW); //Experiment 1 valveB
+      }
+     if (experiment == 1 && origin == 2) // chamberA going out
+     {
+      switch_conn_pin(BASEBD_J9_PIN2, HIGH); //Experiment 1 valveB
+      }
+     if (experiment == 1 && target == 4) 
+     {
+      int move_pulse = liquid_volume / 25; //1ml = 1000uL and the pump moves 35uL at a time
+
+      for (int i = 0; i <= move_pulse; i++){
+      switch_conn_pin(BASEBD_J9_PIN4, HIGH); //Experiment 1 pumpB
+      digitalWrite(LED, HIGH);
+      delay(200);
+      switch_conn_pin(BASEBD_J9_PIN4, LOW); //Experiment 1 pumpB
+      digitalWrite(LED, LOW);
+      delay(200);
+      Serial.println( i * 25);
+      } 
+      switch_conn_pin(BASEBD_J9_PIN2, LOW); //Reset experiment 1 valveB so chamberA is blocked  
+     }
+}
+
+void switchCollection(int experiment, int bag)
+{
+   if (experiment == 1 && bag == 5)
+     {
+      switch_conn_pin(BASEBD_J5_PIN4, LOW); //Experiment 1 waste
+      }
+   if (experiment == 1 && bag == 6)
+     {
+      switch_conn_pin(BASEBD_J5_PIN4, HIGH); //Experiment 1 preservativeOne
+      }
+   
+  }
 
 void Taking_Sensor_Data(){
   Serial.println("Taking Sensor Data");
