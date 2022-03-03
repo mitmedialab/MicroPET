e8bfffc7b41da0aa9c363104e396e2e3f62bea3b
Patrick Chwalek <patrickchwalek@gmail.com>
2022-03-03 13:28:44 -0500

    added RTC code to the dress rehersal sketch

--- Teensey Code/MicroPET_DressRehearsal_202202/MicroPET_DressRehearsal_202202.ino
+++ Teensey Code/MicroPET_DressRehearsal_202202/MicroPET_DressRehearsal_202202.ino
@@ -25,6 +25,7 @@
 #include <Adafruit_BME280.h>
 #include <Adafruit_AS726x.h>
 #include <Adafruit_MCP23X17.h>
+#include "RTClib.h"
 
 //Teensey 4.1 - SCL 19 - yellow /SDA 18 - blue 
 
@@ -36,6 +37,8 @@
 #define BME_MOSI 11
 #define BME_CS 10
 #define SEALEVELPRESSURE_HPA (1013.25)
+
+RTC_PCF8523 rtc;
 
 Adafruit_AS726x ams;
 Adafruit_BME280 bme; // I2C
@@ -213,6 +216,8 @@ void setup() {
     Serial.println(F("Begin Serial"));
     init_card();
 
+    rtcInit();
+    
     pinMode(LED, OUTPUT);
 // uncomment appropriate mcp.begin
   if (!mcp0.begin_I2C(MCP_ADDR0)) {
@@ -298,8 +303,11 @@ void setup() {
     // EXPERIMENTAL SETUP
       
     //STARTING DATE
-    setTime(11,0,0,12,2,2022);  // hr,min,sec,day,mnth,yr
-
+//    setTime(11,0,0,12,2,2022);  // hr,min,sec,day,mnth,yr
+    Serial.println("Setting time from RTC");
+    setTime(rtc.now().unixtime()); //sets time from RTC
+    Serial.print("Time is now: ");
+    Serial.println(now());
 //    //DAY1
 //    Alarm_Day1_ID = Alarm.triggerOnce(tmConvert_t(2022,2,18,11,1,5), day_1); //  YYYY,  MM,  DD,  hh,  mm,  ss
 //    //DAY2
@@ -336,7 +344,52 @@ void loop() {
   }
 }
 
+void rtcInit(){
+    if (! rtc.begin()) { 
+      Serial.println("Couldn't find RTC");
+      Serial.flush();
+    }
+    
+    if (! rtc.initialized() || rtc.lostPower()) {
+      Serial.println("RTC is NOT initialized, let's set the time!");
+      // When time needs to be set on a new device, or after a power loss, the
+      // following line sets the RTC to the date & time this sketch was compiled
+      rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+      // This line sets the RTC with an explicit date & time, for example to set
+      // January 21, 2014 at 3am you would call:
+      // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+      //
+      // Note: allow 2 seconds after inserting battery or applying external power
+      // without battery before calling adjust(). This gives the PCF8523's
+      // crystal oscillator time to stabilize. If you call adjust() very quickly
+      // after the RTC is powered, lostPower() may still return true.
+    }
 
+    // When the RTC was stopped and stays connected to the battery, it has
+    // to be restarted by clearing the STOP bit. Let's do this to ensure
+    // the RTC is running.
+    rtc.start();
+
+       // The PCF8523 can be calibrated for:
+  //        - Aging adjustment
+  //        - Temperature compensation
+  //        - Accuracy tuning
+  // The offset mode to use, once every two hours or once every minute.
+  // The offset Offset value from -64 to +63. See the Application Note for calculation of offset values.
+  // https://www.nxp.com/docs/en/application-note/AN11247.pdf
+  // The deviation in parts per million can be calculated over a period of observation. Both the drift (which can be negative)
+  // and the observation period must be in seconds. For accuracy the variation should be observed over about 1 week.
+  // Note: any previous calibration should cancelled prior to any new observation period.
+  // Example - RTC gaining 43 seconds in 1 week
+  float drift = 43; // seconds plus or minus over oservation period - set to 0 to cancel previous calibration.
+  float period_sec = (7 * 86400);  // total obsevation period in seconds (86400 = seconds in 1 day:  7 days = (7 * 86400) seconds )
+  float deviation_ppm = (drift / period_sec * 1000000); //  deviation in parts per million (μs)
+  float drift_unit = 4.34; // use with offset mode PCF8523_TwoHours
+  // float drift_unit = 4.069; //For corrections every min the drift_unit is 4.069 ppm (use with offset mode PCF8523_OneMinute)
+  int offset = round(deviation_ppm / drift_unit);
+  // rtc.calibrate(PCF8523_TwoHours, offset); // Un-comment to perform calibration once drift (seconds) and observation period (seconds) are correct
+  // rtc.calibrate(PCF8523_TwoHours, 0); // Un-comment to cancel previous calibration
+}
 
 //typedef union {
 //   systemStateStruct state;
